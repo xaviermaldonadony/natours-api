0fcf24c4f84eafb9e14e68c4cfad74fae7f238bd
Xavi <xaviermaldonadony@hotmail.com>
2021-03-25 02:18:43 -0400

    Stripe webhooks updated and bug

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -62,7 +62,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   // const price = session.line_items[0].amount / 100;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({
     tour,
